043bf814cf96805fe1619bfbffc0cfc92237e8a4
Ross Merrigan <email@rossmerrigan.com>
2018-01-03 17:55:45 +0100

    made the benchmarks a bit some detailed

--- concurrentMap_test.go
+++ concurrentMap_test.go
@@ -12,12 +12,24 @@ func BenchmarkMap(b *testing.B) {
 	for i := 0; i < 1000000; i++ {
 		s[i] = i
 	}
+
 }
 
 func BenchmarkConcurrentMap(b *testing.B) {
 	s := GraphBLAS.NewConcurrentMap()
 	for i := 0; i < 1000000; i++ {
 		s.Set(i, i)
+	}
+
+	go func() {
+		for i := 0; i < 1000000; i++ {
+			s.Set(i, i)
+		}
+	}()
+
+	sum := 0
+	for i := range s.Iter() {
+		sum += i.Key
 	}
 }
 
@@ -26,4 +38,14 @@ func BenchmarkSyncMap(b *testing.B) {
 	for i := 0; i < 1000000; i++ {
 		s.Store(i, i)
 	}
+	go func() {
+		for i := 0; i < 1000000; i++ {
+			s.Store(i, i)
+		}
+	}()
+	sum := 0
+	s.Range(func(key interface{}, value interface{}) bool {
+		sum += sum
+		return true
+	})
 }
