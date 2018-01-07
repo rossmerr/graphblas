ea2dbaba27d4160a5f98a16a7652bc8503060bc8
Ross Merrigan <email@rossmerrigan.com>
2018-01-07 17:10:36 +0100

    added a few more matrix functions

--- matrix.go
+++ matrix.go
@@ -2,7 +2,11 @@ package GraphBLAS
 
 type Matrix [][]int
 
-func NewMatrix(x, y int, initialise func([]int, int)) Matrix {
+func NewMatrix(x, y int) Matrix {
+	return newMatrix(x, y, nil)
+}
+
+func newMatrix(x, y int, initialise func([]int, int)) Matrix {
 	m := make(Matrix, x)
 
 	for i := 0; i < x; i++ {
@@ -17,7 +21,7 @@ func NewMatrix(x, y int, initialise func([]int, int)) Matrix {
 
 // Scalar multiplication
 func (m Matrix) Scalar(s int) Matrix {
-	matrix := NewMatrix(len(m), len(m[0]), func(n []int, x int) {
+	matrix := newMatrix(len(m), len(m[0]), func(n []int, x int) {
 		for v := 0; v < len(m[0]); v++ {
 			n[v] = s * m[x][v]
 		}
@@ -26,13 +30,13 @@ func (m Matrix) Scalar(s int) Matrix {
 	return matrix
 }
 
-// Multiply Multiplies a Matrix structure by another Matrix structure.
+// Multiply multiplies a Matrix structure by another Matrix structure.
 func (m Matrix) Multiply(m2 Matrix) (Matrix, bool) {
 	if len(m[0]) != len(m2) {
 		return nil, false
 	}
 
-	matrix := NewMatrix(len(m), len(m2[0]), func(row []int, x int) {
+	matrix := newMatrix(len(m), len(m2[0]), func(row []int, x int) {
 		for j := 0; j < len(m2[0]); j++ {
 			temp := 0
 			for k := 0; k < len(m[0]); k++ {
@@ -43,4 +47,54 @@ func (m Matrix) Multiply(m2 Matrix) (Matrix, bool) {
 	})
 
 	return matrix, true
+}
+
+// Add addition of a Matrix structure by another Matrix structure.
+func (m Matrix) Add(m2 Matrix) (Matrix, bool) {
+	if len(m) != len(m2) {
+		return nil, false
+	}
+
+	if len(m[0]) != len(m2[0]) {
+		return nil, false
+	}
+
+	matrix := newMatrix(len(m), len(m2[0]), func(row []int, x int) {
+		for j := 0; j < len(m2[0]); j++ {
+			row[j] = m[x][j] + m2[x][j]
+		}
+	})
+
+	return matrix, true
+}
+
+// Subtract subtracts one matrix from another.
+func (m Matrix) Subtract(m2 Matrix) (Matrix, bool) {
+	if len(m) != len(m2) {
+		return nil, false
+	}
+
+	if len(m[0]) != len(m2[0]) {
+		return nil, false
+	}
+
+	matrix := newMatrix(len(m), len(m2[0]), func(row []int, x int) {
+		for j := 0; j < len(m2[0]); j++ {
+			row[j] = m[x][j] - m2[x][j]
+		}
+	})
+
+	return matrix, true
+}
+
+// Negative the negative of a matrix.
+func (m Matrix) Negative() Matrix {
+
+	matrix := newMatrix(len(m), len(m[0]), func(row []int, x int) {
+		for j := 0; j < len(m[0]); j++ {
+			row[j] = -m[x][j]
+		}
+	})
+
+	return matrix
 }
--- matrix_test.go
+++ matrix_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestScalar(t *testing.T) {
-	m := GraphBLAS.NewMatrix(2, 2, nil)
+	m := GraphBLAS.NewMatrix(2, 2)
 	m[0][0] = 4
 	m[0][1] = 0
 	m[1][0] = 1
@@ -19,7 +19,7 @@ func TestScalar(t *testing.T) {
 }
 
 func TestMultiple(t *testing.T) {
-	m := GraphBLAS.NewMatrix(2, 3, nil)
+	m := GraphBLAS.NewMatrix(2, 3)
 	m[0][0] = 1
 	m[0][1] = 2
 	m[0][2] = 3
@@ -27,7 +27,7 @@ func TestMultiple(t *testing.T) {
 	m[1][1] = 5
 	m[1][2] = 6
 
-	m2 := GraphBLAS.NewMatrix(3, 2, nil)
+	m2 := GraphBLAS.NewMatrix(3, 2)
 	m2[0][0] = 7
 	m2[0][1] = 8
 	m2[1][0] = 9
@@ -55,5 +55,103 @@ func TestMultiple(t *testing.T) {
 
 	if m3[1][1] != 154 {
 		t.Errorf("Expected 154 got %+v", m3[1][1])
+	}
+}
+
+func TestAdd(t *testing.T) {
+	m := GraphBLAS.NewMatrix(2, 2)
+	m[0][0] = 3
+	m[0][1] = 8
+	m[1][0] = 4
+	m[1][1] = 6
+
+	m2 := GraphBLAS.NewMatrix(2, 2)
+	m2[0][0] = 4
+	m2[0][1] = 0
+	m2[1][0] = 1
+	m2[1][1] = -9
+
+	m3, ok := m.Add(m2)
+
+	if ok == false {
+		t.Error("Add failed")
+	}
+
+	if m3[0][0] != 7 {
+		t.Errorf("Expected 7 got %+v", m3[0][0])
+	}
+
+	if m3[0][1] != 8 {
+		t.Errorf("Expected 8 got %+v", m3[0][1])
+	}
+
+	if m3[1][0] != 5 {
+		t.Errorf("Expected 5 got %+v", m3[1][0])
+	}
+
+	if m3[1][1] != -3 {
+		t.Errorf("Expected -3 got %+v", m3[1][1])
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	m := GraphBLAS.NewMatrix(2, 2)
+	m[0][0] = 3
+	m[0][1] = 8
+	m[1][0] = 4
+	m[1][1] = 6
+
+	m2 := GraphBLAS.NewMatrix(2, 2)
+	m2[0][0] = 4
+	m2[0][1] = 0
+	m2[1][0] = 1
+	m2[1][1] = -9
+
+	m3, ok := m.Subtract(m2)
+
+	if ok == false {
+		t.Error("Add failed")
+	}
+
+	if m3[0][0] != -1 {
+		t.Errorf("Expected -1 got %+v", m3[0][0])
+	}
+
+	if m3[0][1] != 8 {
+		t.Errorf("Expected 8 got %+v", m3[0][1])
+	}
+
+	if m3[1][0] != 3 {
+		t.Errorf("Expected 3 got %+v", m3[1][0])
+	}
+
+	if m3[1][1] != 15 {
+		t.Errorf("Expected 15 got %+v", m3[1][1])
+	}
+}
+
+func TestNegative(t *testing.T) {
+	m := GraphBLAS.NewMatrix(2, 2)
+	m[0][0] = 2
+	m[0][1] = -4
+	m[1][0] = 7
+	m[1][1] = 10
+
+	m3 := m.Negative()
+
+	if m3[0][0] != -2 {
+		t.Errorf("Expected -2 got %+v", m3[0][0])
+	}
+
+	if m3[0][1] != 4 {
+		t.Errorf("Expected 4 got %+v", m3[0][1])
+	}
+
+	if m3[1][0] != -7 {
+		t.Errorf("Expected -7 got %+v", m3[1][0])
+	}
+
+	if m3[1][1] != -10 {
+		t.Errorf("Expected -10 got %+v", m3[1][1])
 	}
 }
