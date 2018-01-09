11319190edac85231b0cced080ac9ac0ec5a2ee2
Ross Merrigan <email@rossmerrigan.com>
2018-01-09 18:21:31 +0100

    testing sets

--- sparseMatrix_test.go
+++ sparseMatrix_test.go
@@ -9,5 +9,10 @@ import (
 func TestSparseMatrix_Set(t *testing.T) {
 	s := GraphBLAS.NewSparseMatrix(3, 3)
 	s.Set(0, 0, 31)
-
+	s.Set(0, 1, 0)
+	s.Set(0, 2, 53)
+	s.Set(1, 0, 0)
+	s.Set(1, 1, 59)
+	s.Set(1, 2, 0)
+	s.Set(2, 0, 41)
 }
