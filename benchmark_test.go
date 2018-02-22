95bb4ccfd1d47ad560aef6ccf2f5e99296c6ec04
Ross Merrigan <email@rossmerrigan.com>
2018-02-22 10:18:29 +0100

    cleanup benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,28 +7,35 @@ import (
 	GraphBLAS "github.com/RossMerr/Caudex.GraphBLAS"
 )
 
-func BenchmarkMatrix(b *testing.B) {
-	for _, fn := range benchmarks {
-		fn.fn(b)
+func BenchmarkMatrixDense(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		c := dense(1)
+		v := c.At(0, 0)
+		if !(v >= 0) {
+			b.Fatal("assert failed")
+		}
+	}
+
+}
+
+func BenchmarkMatrixCSR(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		c := csr(1)
+		v := c.At(0, 0)
+		if !(v >= 0) {
+			b.Fatal("assert failed")
+		}
 	}
 }
 
-var benchmarks = []struct {
-	name string
-	fn   func(*testing.B)
-}{
-	{
-		name: "matrix_multiply_dense",
-		fn: func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				c := dense(1)
-				v := c.At(0, 0)
-				if !(v >= 0) {
-					b.Fatal("assert failed")
-				}
-			}
-		},
-	},
+func BenchmarkMatrixCSC(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		c := csc(1)
+		v := c.At(0, 0)
+		if !(v >= 0) {
+			b.Fatal("assert failed")
+		}
+	}
 }
 
 func dense(n int) GraphBLAS.Matrix {
@@ -56,27 +63,52 @@ func dense(n int) GraphBLAS.Matrix {
 	return c
 }
 
-// func csr(n int) GraphBLAS.Matrix {
-// 	aData := make([][]float64, n)
-// 	for r := range aData {
-// 		aData[r] = make([]float64, n)
-// 		for c := range aData {
-// 			aData[r][c] = rand.Float64()
+func csr(n int) GraphBLAS.Matrix {
+	aData := make([][]float64, n)
+	for r := range aData {
+		aData[r] = make([]float64, n)
+		for c := range aData {
+			aData[r][c] = rand.Float64()
 
-// 		}
-// 	}
-// 	a := GraphBLAS.NewCSRMatrix(aData)
+		}
+	}
+	a := GraphBLAS.NewCSRMatrixFromArray(aData)
 
-// 	bData := make([][]float64, n)
-// 	for r := range bData {
-// 		bData[r] = make([]float64, n)
-// 		for c := range bData {
-// 			bData[r][c] = rand.Float64()
+	bData := make([][]float64, n)
+	for r := range bData {
+		bData[r] = make([]float64, n)
+		for c := range bData {
+			bData[r][c] = rand.Float64()
 
-// 		}
-// 	}
-// 	b := GraphBLAS.NewCSRMatrix(bData)
+		}
+	}
+	b := GraphBLAS.NewCSRMatrixFromArray(bData)
 
-// 	c := a.Multiply(b)
-// 	return c
-// }
+	c := a.Multiply(b)
+	return c
+}
+
+func csc(n int) GraphBLAS.Matrix {
+	aData := make([][]float64, n)
+	for r := range aData {
+		aData[r] = make([]float64, n)
+		for c := range aData {
+			aData[r][c] = rand.Float64()
+
+		}
+	}
+	a := GraphBLAS.NewCSCMatrixFromArray(aData)
+
+	bData := make([][]float64, n)
+	for r := range bData {
+		bData[r] = make([]float64, n)
+		for c := range bData {
+			bData[r][c] = rand.Float64()
+
+		}
+	}
+	b := GraphBLAS.NewCSCMatrixFromArray(bData)
+
+	c := a.Multiply(b)
+	return c
+}
